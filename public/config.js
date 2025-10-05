c435a6631f9cceae9b7b36cc6456333e55ef31ca
anaretete <htc.lemon1@gmail.com>
2025-10-05 00:49:14 -0400

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "https://schoolstreamproxy.netlify.app/",
+  VITE_CORS_PROXY_URL: "https://schoolstreamproxy.netlify.app",
 
   // The READ API key to access TMDB
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OThlNmJiZTRmYTA0NjQxODBkN2FkMWIwNjUyOTIyMyIsIm5iZiI6MTc1OTYzNDQ4NS41MDIsInN1YiI6IjY4ZTFlNDM1MTAzOTU1YWE5OWY4MTJhNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QVfU_CJ2ge5GzGFvO4rM_Yd80sKf3NK5ZoCO6sTskrs",
