fd9fc9b6efcf47f4e0ef0cb08f8a1874c1552866
anaretete <htc.lemon1@gmail.com>
2025-10-04 23:45:31 -0400

    Update config.js

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@ window.__CONFIG__ = {
   VITE_CORS_PROXY_URL: "",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OThlNmJiZTRmYTA0NjQxODBkN2FkMWIwNjUyOTIyMyIsIm5iZiI6MTc1OTYzNDQ4NS41MDIsInN1YiI6IjY4ZTFlNDM1MTAzOTU1YWE5OWY4MTJhNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QVfU_CJ2ge5GzGFvO4rM_Yd80sKf3NK5ZoCO6sTskrs",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
